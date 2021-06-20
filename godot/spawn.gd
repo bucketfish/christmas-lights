0590391ea0d944684e68e9b0d72faff366ac3330
ty <fqdingsky@gmail.com>
2021-06-20 12:35:21 +0800

    even better tileset

--- godot/spawn.gd
+++ godot/spawn.gd
@@ -2,13 +2,3 @@ extends Position2D
 
 
 export var on: bool
-onready var player = $player
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
