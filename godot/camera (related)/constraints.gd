fbf726d7f8f734c93cb5c2c1fb93ca9626943929
ty <fqdingsky@gmail.com>
2021-07-14 20:47:49 +0800

    some light things! yay

--- godot/camera (related)/constraints.gd
+++ godot/camera (related)/constraints.gd
@@ -4,6 +4,7 @@ onready var camera = get_node("/root/game/camera")
 onready var topleft = $topleft
 onready var bottomright = $bottomright
 
+export var on = true
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,10 +12,11 @@ onready var bottomright = $bottomright
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	camera.limit_top = topleft.position.y
-	camera.limit_left = topleft.position.x
-	camera.limit_bottom = bottomright.position.y
-	camera.limit_right = bottomright.position.x
+	if on:
+		camera.limit_top = topleft.position.y
+		camera.limit_left = topleft.position.x
+		camera.limit_bottom = bottomright.position.y
+		camera.limit_right = bottomright.position.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
