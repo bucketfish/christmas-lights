af1c3eb923f33475e21b48373b0fe7dee1c223aa
ty <fqdingsky@gmail.com>
2021-08-04 10:20:51 +0800

    saving function to gate. i should refactor load,

--- godot/decorations-elements/button/button.gd
+++ godot/decorations-elements/button/button.gd
@@ -25,3 +25,4 @@ func _on_Area2D_body_entered(body):
 		emit_signal("button_press")
 		$AnimationPlayer.play("press")
 		pressed = true
+
--- godot/decorations-elements/gate/gate.gd
+++ godot/decorations-elements/gate/gate.gd
@@ -4,14 +4,16 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export var scene_id:String
 
 var open = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	open = false
-	$gate/AnimationPlayer.play("idle")
-	pass # Replace with function body.
+	if open:
+		$gate/AnimationPlayer.play("idle")
+	else:
+		$gate/AnimationPlayer.play("open")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,3 +25,9 @@ func _on_button_press():
 	if !open:
 		open = true
 		$gate/AnimationPlayer.play("open")
+
+func save():
+	var save_d = {
+		"open": open
+	}
+	return save_d
